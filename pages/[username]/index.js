9e32db88d4fd1e11e8abfc216b56fa198b628179
roesnera <adam.roesner@generaiton.org>
2022-09-28 10:29:07 -0400

    Pages setup, unique username with db mapping done

--- pages/[username]/index.js
+++ pages/[username]/index.js
@@ -1,6 +1,45 @@
-export default function UserPage({ }) {
+import UserProfile from '../../components/UserProfile';
+import PostFeed from '../../components/PostFeed';
+import { getUserWithUsername } from '../../lib/firebase';
+
+export async function getServerSideProps({ query }){
+  const { username } = query;
+
+  const userDoc = await getUserWithUsername(username);
+
+  let user = null;
+  let posts = null;
+
+  if (userDoc) {
+    user = userDoc.data();
+    const postsQuery = userDoc.ref
+    .collection('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(5);
+
+    posts = (await postsQuery.get()).docs.map(postToJSON);
+  }
+
+  return {
+    props: {user, posts },
+  };
+}
+
+export default function UserPage({ user, posts }) {
   return (
     <main>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
     </main>
   )
+}
+
+export function postToJSON(doc) {
+  const data = doc.data();
+  return {
+    ...data,
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis()
+  }
 }
